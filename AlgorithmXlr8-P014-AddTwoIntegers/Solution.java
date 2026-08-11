7d2e722d10c5e2f9bbe7e12cd2e5113523e62c25
Nehal Panchal <nehalpanchal.in@gmail.com>
2026-08-11 23:42:35 +0530

    Solve Add Two Integers

---
+++ AlgorithmXlr8-P014-AddTwoIntegers/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+
+        System.out.print(num1 + num2);
+    }
+}
