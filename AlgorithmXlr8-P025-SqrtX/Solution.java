6041f7ec1a57d1c60fafbf0e69b0516af1b605b8
Nehal Panchal <nehalpanchal.in@gmail.com>
2026-08-27 12:33:06 +0530

    Solve Sqrt(x)

---
+++ AlgorithmXlr8-P025-SqrtX/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+
+        int y = (int) Math.sqrt(x);
+        System.out.print(y);
+
+
+    }
+}
